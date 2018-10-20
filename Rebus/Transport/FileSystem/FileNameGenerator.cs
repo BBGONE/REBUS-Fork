8cdeb5437cf41555ba9463dadc7843fc779f15f2
Maxim T <lptscsi@gmail.com>
2018-10-20 07:11:42 +0300

    FileSystemTransport Code refactory

---
+++ Rebus/Transport/FileSystem/FileNameGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Rebus.Transport.FileSystem
+{
+    class FileNameGenerator
+    {
+        private int _incrementingCounter = 0;
+        readonly string _transportId;
+
+        public FileNameGenerator() {
+            // Generate unique transport id
+            _transportId = GenerateID();
+        }
+
+        static class RandomLetter
+        {
+            static Random _random = new Random(Guid.NewGuid().GetHashCode());
+            public static char GetLetter()
+            {
+                char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+                int num = _random.Next(0, chars.Length); // Zero to chars.Length -1
+                return chars[num];
+            }
+        }
+
+        private static string GenerateID()
+        {
+            var charsToRemove = new char[] { '/', '+', '=' };
+            var replacement = new char[] { RandomLetter.GetLetter(), RandomLetter.GetLetter(), RandomLetter.GetLetter() };
+            string str = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 22);
+            int len = str.Length;
+            for (int i = 0; i < charsToRemove.Length; ++i)
+            {
+                str = str.Replace(charsToRemove[i], replacement[i]);
+            }
+            return str;
+        }
+
+        public string GetNextFileName()
+        {
+            Interlocked.CompareExchange(ref _incrementingCounter, 0, 99999);
+            string ticks = (DateTime.Now.Ticks - FileSystemHelper.historicalDate.Ticks).ToString().PadLeft(19, '0');
+            string seqnum = Interlocked.Increment(ref _incrementingCounter).ToString().PadLeft(5, '0');
+            return $"b{ticks}{seqnum}_{_transportId}.json";
+        }
+    }
+}
---
+++ Rebus/Transport/FileSystem/QueueRegister.cs
@@ -0,0 +1,68 @@
+﻿using Rebus.Exceptions;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace Rebus.Transport.FileSystem
+{
+    class QueueRegister
+    {
+        readonly ConcurrentDictionary<string, string> _initializedQueues;
+        readonly string _baseDirectory;
+        readonly object _queueInitLock = new object();
+
+        public QueueRegister(string baseDirectory)
+        {
+            _baseDirectory = baseDirectory;
+            _initializedQueues = new ConcurrentDictionary<string, string>();
+        }
+
+        public string BaseDirectory => _baseDirectory;
+
+        public string EnsureQueueInitialized(string queueName)
+        {
+            string directory;
+            if (_initializedQueues.TryGetValue(queueName, out directory))
+                return directory;
+
+            lock (this._queueInitLock)
+            {
+                // double check to prevent race
+                if (_initializedQueues.TryGetValue(queueName, out directory))
+                    return directory;
+
+                directory = GetDirectoryForQueueNamed(queueName);
+
+                if (Directory.Exists(directory))
+                {
+                    _initializedQueues.TryAdd(queueName, directory);
+                    return directory;
+                }
+
+                Exception caughtException = null;
+                try
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                catch (Exception exception)
+                {
+                    caughtException = exception;
+                }
+
+                if (caughtException != null && !Directory.Exists(directory))
+                {
+                    throw new RebusApplicationException(
+                        caughtException, $"Could not initialize directory '{directory}' for queue named '{queueName}'");
+                }
+
+                _initializedQueues.TryAdd(queueName, directory);
+                return directory;
+            }
+        }
+
+        public string GetDirectoryForQueueNamed(string queueName)
+        {
+            return Path.Combine(_baseDirectory, queueName);
+        }
+    }
+}
